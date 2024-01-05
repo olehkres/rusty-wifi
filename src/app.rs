f1cd9de5999c9df1da2623c65d3182759efa1167
olehkres <krys.oleg@ukr.net>
2024-01-05 12:27:30 +0200

    Separate tasks from spawns and handle errors

--- src/app.rs
+++ src/app.rs
@@ -59,57 +59,76 @@ impl App {
     }
 
     /// Opens native file dialog to select json formatted Access Points list.
-    fn open_file(&mut self) {
+    fn open_file_spawn(&mut self) {
         let aps_p = Arc::clone(&self.aps);
 
         tokio::spawn(async move {
-            let file_join = tokio::spawn(async move {
-                FileDialog::new()
-                    .add_filter("json", &["json"])
-                    .set_location("~")
-                    .show_open_single_file()
-            });
-
-            if let Some(path) = file_join.await?? {
-                let file = File::open(path)?;
-                let reader = BufReader::new(file);
-                let aps: Vec<AccessPoint> = serde_json::from_reader(reader)?;
-                let aps: Vec<AccessPointGUI> = aps.into_iter().map(AccessPointGUI::new).collect();
-                let mut aps_lock = aps_p.lock().await;
-                *aps_lock = aps;
-            };
-
-            Result::<(), AppError>::Ok(())
+            match App::open_file().await {
+                Ok(Some(aps)) => {
+                    let mut aps_lock = aps_p.lock().await;
+                    *aps_lock = aps;
+                }
+                // If user closed window than no message needs to be displayed
+                Ok(None) => (),
+                Err(e) => e.show(),
+            }
         });
+    }
+
+    async fn open_file() -> Result<Option<Vec<AccessPointGUI>>, AppError> {
+        let file_join = tokio::spawn(async move {
+            FileDialog::new()
+                .add_filter("json", &["json"])
+                .set_location("~")
+                .show_open_single_file()
+        });
+
+        if let Some(path) = file_join.await?? {
+            let file = File::open(path)?;
+            let reader = BufReader::new(file);
+            let aps: Vec<AccessPoint> = serde_json::from_reader(reader)?;
+            let aps: Vec<AccessPointGUI> = aps.into_iter().map(AccessPointGUI::new).collect();
+
+            return Ok(Some(aps));
+        };
+
+        Ok(None)
     }
 
     /// Opens native file dialog to select path and name for file
     /// of json formatted Access Points list.
-    fn save_file(&self) {
-        let wifis = Arc::clone(&self.aps);
+    fn save_file_spawn(&self) {
+        let aps_p = Arc::clone(&self.aps);
 
         tokio::spawn(async move {
-            let file_join = tokio::spawn(async move {
-                FileDialog::new()
-                    .add_filter("json", &["json"])
-                    .set_location("~")
-                    .show_save_single_file()
-            });
-
-            let serder_aps = tokio::spawn(async move {
-                let lock = wifis.lock().await;
-                let aps: Vec<&AccessPoint> = lock.iter().map(|w| w.raw()).collect();
-                Result::<Vec<u8>, AppError>::Ok(serde_json::to_vec(&aps)?)
-            });
-
-            let path = file_join.await??;
-
-            if let Some(path) = path {
-                let mut file = File::create(path)?;
-                file.write_all(&serder_aps.await??)?;
-            };
-
-            Result::<(), AppError>::Ok(())
+            match Self::save_file(aps_p).await {
+                Ok(()) => (),
+                Err(e) => e.show(),
+            }
         });
+    }
+
+    async fn save_file<'a>(aps_p: Arc<Mutex<Vec<AccessPointGUI>>>) -> Result<(), AppError> {
+        let file_join = tokio::spawn(async move {
+            FileDialog::new()
+                .add_filter("json", &["json"])
+                .set_location("~")
+                .show_save_single_file()
+        });
+
+        let serder_aps = tokio::spawn(async move {
+            let aps_lock = aps_p.lock().await;
+            let aps_raw: Vec<&AccessPoint> = aps_lock.iter().map(|w| w.raw()).collect();
+            Result::<Vec<u8>, AppError>::Ok(serde_json::to_vec(&aps_raw)?)
+        });
+
+        let path = file_join.await??;
+
+        if let Some(path) = path {
+            let mut file = File::create(path)?;
+            file.write_all(&serder_aps.await??)?;
+        };
+
+        Ok(())
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,32 @@
+use std::error::Error;
+
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum AppError {
     #[error("Thread error!")]
     Tokio(#[from] tokio::task::JoinError),
-    #[error("Serde error!")]
+    #[error("Serialize/Deserialize error!")]
     Serde(#[from] serde_json::Error),
     #[error("Input/output error!")]
     IO(#[from] std::io::Error),
     #[error("Native dialog error")]
     NativeDialog(#[from] native_dialog::Error),
+}
+
+impl AppError {
+    pub fn show(&self) {
+        match native_dialog::MessageDialog::new()
+            .set_title("Error!")
+            .set_text(&format!(
+                "Eror: \"{}\". Caused by: \"{}\"",
+                self,
+                self.source().unwrap()
+            ))
+            .show_alert()
+        {
+            Ok(()) => (),
+            Err(e) => println!("{}", e),
+        }
+    }
 }
